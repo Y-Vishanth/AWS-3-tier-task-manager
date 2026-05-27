fa9bad38d8d25cf74d5a245d58dfd2f8d543db35
Ubuntu <ubuntu@ip-172-31-4-227.ec2.internal>
2026-05-27 14:51:17 +0000

    Added the CORS configuration

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -10,7 +11,16 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: [
+    'http://localhost:5173',
+    'https://d4ouj8fq8dbb1.cloudfront.net'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}
+
+app.use(cors(corsOptions))
 
 app.use(express.json());
 
@@ -27,3 +37,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
+
